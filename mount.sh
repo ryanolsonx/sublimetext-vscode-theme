8755e8f94a959b845a11e7b803ea825ac27c43f8
Ryan Olson <ryolson@me.com>
2022-06-12 21:27:50 -0600

    Mount it

---
+++ mount.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm -rf ~/Library/Application\ Support/Sublime\ Text/Packages/User/vscode
+ln -s ~/r/sublimetext-vscode-theme ~/Library/Application\ Support/Sublime\ Text/Packages/User/vscode
